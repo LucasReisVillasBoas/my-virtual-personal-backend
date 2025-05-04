e24081123c99bf7a19c46e15794fedd28bbaa742
brunasantander <brunasantander.2011@gmail.com>
2025-05-04 20:09:29 -0300

    🎨 Separate muscle groupes
    separating legs muscles in groups

---
+++ src/database/migrations/Migration20250504205916.ts
@@ -0,0 +1,18 @@
+import { Migration } from '@mikro-orm/migrations';
+
+export class Migration20250504205916 extends Migration {
+
+  override async up(): Promise<void> {
+    this.addSql(`alter table "training" add column "user_id" uuid not null;`);
+    this.addSql(`alter table "training" add constraint "training_user_id_foreign" foreign key ("user_id") references "user" ("id") on update cascade;`);
+    this.addSql(`create index "training_user_id_index" on "training" ("user_id");`);
+  }
+
+  override async down(): Promise<void> {
+    this.addSql(`alter table "training" drop constraint "training_user_id_foreign";`);
+
+    this.addSql(`drop index "training_user_id_index";`);
+    this.addSql(`alter table "training" drop column "user_id";`);
+  }
+
+}
