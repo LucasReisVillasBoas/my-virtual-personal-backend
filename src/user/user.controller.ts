437a3af6827bc73e37a6ebb0c6e42dc6a5528896
brunasantander <brunasantander.2011@gmail.com>
2025-05-05 19:45:01 -0300

    ✨ User CRUD
    adding users crud

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,9 +1,19 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Post,
+  Put,
+  Query,
+} from '@nestjs/common';
 import { ApiResponse, ApiTags } from '@nestjs/swagger';
 import { UserService } from './user.service';
 import { UserRegisterRequestDto } from './dto/user-register-request.dto';
 import { UserRegisterResponseDto } from './dto/user-register-response.dto';
 import { filterUserFields } from '../utils/user.util';
+import { UserResponseDto } from 'src/user/dto/user-response-dto';
+import { User } from 'src/entities/user/user.entity';
 
 @Controller('user')
 @ApiTags('User')
@@ -28,5 +38,69 @@ export class UserController {
     );
 
     return userRegisterResponseDto;
+  }
+
+  @ApiResponse({
+    status: 200,
+    type: UserRegisterResponseDto,
+    description: 'User found',
+  })
+  @Get('/')
+  async getById(@Query('id') id: string) {
+    const user = await this.userService.getById(id);
+
+    return new UserRegisterResponseDto('User found', 200, {
+      user: filterUserFields(user, []),
+    });
+  }
+
+  @ApiResponse({
+    status: 200,
+    type: Array<UserRegisterResponseDto>,
+    description: 'Users found',
+  })
+  @Get('/all')
+  async getAll() {
+    const userList = await this.userService.getAll();
+
+    return userList.map((user) => {
+      return new UserRegisterResponseDto('Users found', 200, {
+        user: filterUserFields(user, []),
+      });
+    });
+  }
+
+  @ApiResponse({
+    status: 200,
+    type: UserResponseDto,
+    description: 'User updated successfully',
+  })
+  @Put('/')
+  async update(
+    @Query('id') id: string,
+    @Body() userRegisterRequestDto: Partial<User>,
+  ) {
+    const userUpdated = await this.userService.update(
+      id,
+      userRegisterRequestDto,
+    );
+
+    return new UserResponseDto('User updated successfully', 200, {
+      data: filterUserFields(userUpdated, []),
+    });
+  }
+
+  @ApiResponse({
+    status: 200,
+    type: UserResponseDto,
+    description: 'User deleted successfully',
+  })
+  @Delete('/')
+  async delete(@Query('id') id: string) {
+    const userUpdated = await this.userService.delete(id);
+
+    return new UserResponseDto('User deleted successfully', 200, {
+      data: { userDeleted: userUpdated },
+    });
   }
 }
